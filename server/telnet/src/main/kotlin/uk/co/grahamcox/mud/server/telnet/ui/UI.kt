6b7bd02ea21f61f6cd25ec25b26647442cca63ba
Graham Cox <graham@grahamcox.co.uk>
2016-03-08 12:55:16 +0000

    Keep track of the status of the interesting options so we know when to react

--- server/telnet/src/main/kotlin/uk/co/grahamcox/mud/server/telnet/ui/UI.kt
+++ server/telnet/src/main/kotlin/uk/co/grahamcox/mud/server/telnet/ui/UI.kt
@@ -4,7 +4,9 @@ import io.netty.channel.socket.SocketChannel
 import org.slf4j.LoggerFactory
 import uk.co.grahamcox.mud.server.telnet.options.NAWSOption
 import uk.co.grahamcox.mud.server.telnet.options.OptionManager
+import uk.co.grahamcox.mud.server.telnet.options.TelnetOption
 import uk.co.grahamcox.mud.server.telnet.options.TerminalTypeOption
+import java.util.*
 
 /**
  * The actual entrypoint for the user interface
@@ -17,13 +19,69 @@ class UI(private val optionManager: OptionManager,
     /** The logger to use */
     private val LOG = LoggerFactory.getLogger(UI::class.java)
 
+    /**
+     * Enumeration of the status of a Telnet option that we care about
+     */
+    enum class OptionStatus {
+        /** We don't yet know the status */
+        UNKNOWN,
+        /** The option is disabled */
+        DISABLED,
+        /** The option expired before we got a response */
+        EXPIRED,
+        /** The option is enabled */
+        ENABLED,
+        /** The option is fully configured */
+        CONFIGURED
+    }
+
+    /** Collection of the Option Statuses that we consider to be terminals. I.e. we now know what we're doing with this option */
+    private val TERMINAL_OPTION_STATUSES = EnumSet.of(OptionStatus.DISABLED, OptionStatus.CONFIGURED, OptionStatus.EXPIRED)
+
     init {
-        optionManager.getServerOption(NAWSOption::class.java).eventManager.registerListener(NAWSOption.WINDOW_SIZE_CHANGED_EVENT) {
-            e -> windowSizeChanged(e.payload as NAWSOption.WindowSizePayload)
+        with(optionManager.getServerOption(NAWSOption::class.java).eventManager) {
+            registerListener(TelnetOption.STATE_CHANGED_EVENT) {
+                e -> windowSizeStatusChanged(e.payload as TelnetOption.OptionState)
+            }
+            registerListener(NAWSOption.WINDOW_SIZE_CHANGED_EVENT) {
+                e -> windowSizeChanged(e.payload as NAWSOption.WindowSizePayload)
+            }
         }
-        optionManager.getServerOption(TerminalTypeOption::class.java).eventManager.registerListener(TerminalTypeOption.TERMINAL_TYPE_CHANGED_EVENT) {
-            e -> terminalTypeChanged(e.payload as String)
+
+        with(optionManager.getServerOption(TerminalTypeOption::class.java).eventManager) {
+            registerListener(TelnetOption.STATE_CHANGED_EVENT) {
+                e -> terminalTypeStatusChanged(e.payload as TelnetOption.OptionState)
+            }
+            registerListener(TerminalTypeOption.TERMINAL_TYPE_CHANGED_EVENT) {
+                e -> terminalTypeChanged(e.payload as String)
+            }
         }
+    }
+
+    /** The status of the Window Size option */
+    private var windowSizeOptionStatus: OptionStatus = OptionStatus.UNKNOWN
+
+    /** The actual Window Size to use */
+    private var windowSize: NAWSOption.WindowSizePayload? = null
+
+    /** The status of the Terminal Type option */
+    private var terminalTypeOptionStatus: OptionStatus = OptionStatus.UNKNOWN
+
+    /** The actual Terminal Type to use */
+    private var terminalType: String? = null
+
+    /**
+     * Handle the fact that the status of the Window Size option has changed
+     * @param newStatus The new status of the Window Size option
+     */
+    private fun windowSizeStatusChanged(newStatus: TelnetOption.OptionState) {
+        windowSizeOptionStatus = when (newStatus) {
+            TelnetOption.OptionState.DISABLED -> OptionStatus.DISABLED
+            TelnetOption.OptionState.ENABLED -> OptionStatus.ENABLED
+            TelnetOption.OptionState.UNKNOWN -> OptionStatus.UNKNOWN
+        }
+        windowSize = null
+        handleOptionChanges()
     }
 
     /**
@@ -31,6 +89,23 @@ class UI(private val optionManager: OptionManager,
      * @param windowSize The new window size
      */
     private fun windowSizeChanged(windowSize: NAWSOption.WindowSizePayload) {
+        windowSizeOptionStatus = OptionStatus.CONFIGURED
+        this.windowSize = windowSize
+        handleOptionChanges()
+    }
+
+    /**
+     * Handle the fact that the status of the Terminal Type option has changed
+     * @param newStatus The new status of the Terminal Type option
+     */
+    private fun terminalTypeStatusChanged(newStatus: TelnetOption.OptionState) {
+        terminalTypeOptionStatus = when (newStatus) {
+            TelnetOption.OptionState.DISABLED -> OptionStatus.DISABLED
+            TelnetOption.OptionState.ENABLED -> OptionStatus.ENABLED
+            TelnetOption.OptionState.UNKNOWN -> OptionStatus.UNKNOWN
+        }
+        terminalType = null
+        handleOptionChanges()
     }
 
     /**
@@ -38,5 +113,24 @@ class UI(private val optionManager: OptionManager,
      * @param terminalType The terminal type
      */
     private fun terminalTypeChanged(terminalType: String) {
+        terminalTypeOptionStatus = OptionStatus.CONFIGURED
+        this.terminalType = terminalType
+        handleOptionChanges()
+    }
+
+    /**
+     * Handle when the options change.
+     * This is primarially used to signal the fact that we know what we're talking about with regards to the terminal
+     * and so we can get on with the processing of the game
+     */
+    private fun handleOptionChanges() {
+        LOG.debug("Window Size Status: {}", windowSizeOptionStatus)
+        LOG.debug("Window Size: {}", windowSize)
+        LOG.debug("Terminal Type Status: {}", terminalTypeOptionStatus)
+        LOG.debug("Terminal Type: {}", terminalType)
+
+        if (TERMINAL_OPTION_STATUSES.contains(windowSizeOptionStatus) && TERMINAL_OPTION_STATUSES.contains(terminalTypeOptionStatus)) {
+            LOG.debug("Received full configuration of session")
+        }
     }
 }

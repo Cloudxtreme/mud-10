e06d49c178e475490a27481e3cab132139da83c6
Graham Cox <graham@grahamcox.co.uk>
2016-03-11 21:39:55 +0000

    Stripped out the SuppressGoAhead option for now, and pass the Option Manager to the renderer

--- server/telnet/src/main/kotlin/uk/co/grahamcox/mud/server/telnet/ui/renderer/standard/StandardRendererFactory.kt
+++ server/telnet/src/main/kotlin/uk/co/grahamcox/mud/server/telnet/ui/renderer/standard/StandardRendererFactory.kt
@@ -1,6 +1,7 @@
 package uk.co.grahamcox.mud.server.telnet.ui.renderer.standard
 
 import io.netty.channel.socket.SocketChannel
+import uk.co.grahamcox.mud.server.telnet.options.OptionManager
 import uk.co.grahamcox.mud.server.telnet.ui.UIConfigOption
 import uk.co.grahamcox.mud.server.telnet.ui.renderer.Renderer
 import uk.co.grahamcox.mud.server.telnet.ui.renderer.RendererFactory
@@ -8,8 +9,10 @@ import uk.co.grahamcox.mud.server.telnet.ui.renderer.RendererFactory
 /**
  * Factory to produce the standard renderer
  * @property channel The channel to use for the renderer
+ * @property optionManager The option manager to get the connection options from
  */
-class StandardRendererFactory(private val channel: SocketChannel) : RendererFactory {
+class StandardRendererFactory(private val channel: SocketChannel,
+                              private val optionManager: OptionManager) : RendererFactory {
     /**
      * Create the standard renderer
      * @param configOptions The configuration options for the renderer

5ac5ae8ec5040382de496f3600e6a65f80f97418
Graham Cox <graham@grahamcox.co.uk>
2016-03-14 12:52:04 +0000

    Get received bytes from the client into the UI class

--- server/telnet/src/main/kotlin/uk/co/grahamcox/mud/server/telnet/netty/MudServerHandler.kt
+++ server/telnet/src/main/kotlin/uk/co/grahamcox/mud/server/telnet/netty/MudServerHandler.kt
@@ -4,6 +4,7 @@ import io.netty.channel.ChannelHandlerContext
 import io.netty.channel.ChannelInboundHandlerAdapter
 import io.netty.util.ReferenceCountUtil
 import org.slf4j.LoggerFactory
+import uk.co.grahamcox.mud.server.telnet.TelnetMessage
 import uk.co.grahamcox.mud.server.telnet.ui.UI
 
 /**
@@ -11,11 +12,18 @@ import uk.co.grahamcox.mud.server.telnet.ui.UI
  * @property ui The User Interface that is representing the client
  */
 class MudServerHandler(private val ui: UI) : ChannelInboundHandlerAdapter() {
+    /** The logger to use */
     private val LOG = LoggerFactory.getLogger(MudServerHandler::class.java)
 
     override fun channelRead(ctx: ChannelHandlerContext, msg: Any) {
         try {
-            LOG.info("Received message {} on {}", msg, ctx)
+            LOG.trace("Received message {} on {}", msg, ctx)
+            when (msg) {
+                is TelnetMessage.ByteMessage -> ui.receiveByte(msg.byte)
+                else -> {
+                    LOG.warn("Received a message that we can't handle: {}", msg)
+                }
+            }
         } finally {
             ReferenceCountUtil.release(msg)
         }
